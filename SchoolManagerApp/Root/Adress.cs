a003eea80e87b21375a7434ad4905104f6cb3d8f
Bao Ngoc Duong <2359970@bdeb.qc.ca>
2025-10-08 23:21:03 -0400

    ajout de null safety

--- SchoolManagerApp/Root/Adress.cs
+++ SchoolManagerApp/Root/Adress.cs
@@ -3,20 +3,20 @@ namespace SchoolManager
     public class Address
     {
         public int StreetNumber { get; set; }
-        public string StreetName { get; set; }
-        public string City { get; set; }
-        public string Province { get; set; }
-        public string PostalCode { get; set; }
-        public string Country { get; set; }
+        public string StreetName { get; set; } = string.Empty;
+        public string City { get; set; } = string.Empty;
+        public string Province { get; set; } = string.Empty;
+        public string PostalCode { get; set; } = string.Empty;
+        public string Country { get; set; } = string.Empty;
 
         public Address(int streetNumber, string streetName, string city, string province, string postalCode, string country)
         {
             this.StreetNumber = streetNumber;
-            this.StreetName = streetName;
-            this.City = city;
-            this.Province = province;
-            this.PostalCode = postalCode;
-            this.Country = country;
+            this.StreetName = streetName ?? throw new ArgumentNullException(nameof(streetName));
+            this.City = city ?? throw new ArgumentNullException(nameof(city));
+            this.Province = province ?? throw new ArgumentNullException(nameof(province));
+            this.PostalCode = postalCode ?? throw new ArgumentNullException(nameof(postalCode));
+            this.Country = country ?? throw new ArgumentNullException(nameof(country));
         }
 
         public override string ToString()
