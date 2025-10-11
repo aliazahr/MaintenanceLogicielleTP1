518d0eddb4c046aeb6237091ba209b27101e36b4
aliazahr <aali86362@outlook.com>
2025-10-11 15:04:34 -0400

    Changing folder for a better organisation Correcting writing convention Made fields private

---
+++ SchoolManagerApp/Members/SchoolMember.cs
@@ -0,0 +1,34 @@
+﻿namespace SchoolManager
+{
+    public class SchoolMember
+    {
+        private string _name;
+        private Address? _address;
+        private string _phone;
+
+        public SchoolMember(string name = "", Address? address = null, string phone = "")
+        {
+            Name = name;
+            Address = address;
+            Phone = phone;
+        }
+
+        public string Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        public Address? Address
+        {
+            get => _address;
+            set => _address = value;
+        }
+
+        public string Phone
+        {
+            get => _phone;
+            set => _phone = value;
+        }
+    }
+} 
