aa3aafd76a4216632aaeab8e55f2e8bbe691cbad
aliazahr <aali86362@outlook.com>
2025-10-11 23:47:46 -0400

    Correcting code smells and refactoring

--- SchoolManagerApp/Members/Teacher.cs
+++ SchoolManagerApp/Members/Teacher.cs
@@ -7,19 +7,27 @@ namespace SchoolManager
         private string _subject;
         private int _income;
 
-        public Teacher(string name, Address? address, int phoneNum, string subject = "", int income = 25000)
+        public Teacher(string name, Address address, string phoneNumber, string subject = "", int income = 25000) 
+            : base(name, address, phoneNumber, income)
         {
-            Name = name;
-            Address = address  ?? throw new ArgumentNullException(nameof(address));
-            Phone = phoneNum;
             Subject = subject;
-            this.income = income;
-            balance = 0;
+            Income = income;
+        }
+        public string Subject
+        {
+            get => _subject;
+            set { _subject = value; }
+        }   
+
+        public int Income
+        {
+            get => _income;
+            private set { _income = value; }
         }
 
-        public void display()
+        public override string ToString()
         {
-            Console.WriteLine("Name: {0}, Address: {1}, Phone: {2}, Subject: {3}", Name, Address, Phone, Subject);
+            return $"Teacher: {Name}, Address: {Address}, Phone: {PhoneNumber}, Subject: {Subject}, Income: {Income}, Total Earnings: {TotalEarnings}";
         }
     }
 }
