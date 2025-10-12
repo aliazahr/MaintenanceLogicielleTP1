87c732369d8c49a50c98b3d6315a44af93bc962d
aliazahr <aali86362@outlook.com>
2025-10-12 00:25:34 -0400

    implementing defensive programming in class: null checks

--- SchoolManagerApp/Members/Teacher.cs
+++ SchoolManagerApp/Members/Teacher.cs
@@ -4,25 +4,37 @@ namespace SchoolManager
 {
     public class Teacher : SchoolEmployee, IPayroll
     {
+        private const int DefaultIncome = 25000;
         private string _subject;
         private int _income;
 
-        public Teacher(string name, Address address, string phoneNumber, string subject = "", int income = 25000) 
+        public Teacher(string name, Address address, string phoneNumber, string subject, int income = 25000) 
             : base(name, address, phoneNumber, income)
         {
             Subject = subject;
             Income = income;
         }
+        
         public string Subject
         {
             get => _subject;
-            set { _subject = value; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    throw new ArgumentException("Subject cannot be empty.", nameof(value));
+                _subject = value;
+            }
         }   
 
         public int Income
         {
             get => _income;
-            private set { _income = value; }
+            private set
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException(nameof(value), "Income cannot be negative.");
+                _income = value;
+            }
         }
 
         public override string ToString()
