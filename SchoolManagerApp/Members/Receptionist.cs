b0ac4b2a8cec4c8e632b263bc2cd7db3bc72258b
aliazahr <aali86362@outlook.com>
2025-10-12 01:09:41 -0400

    Correct complaint class mistake and implementation, fixing code smells

--- SchoolManagerApp/Members/Receptionist.cs
+++ SchoolManagerApp/Members/Receptionist.cs
@@ -10,7 +10,7 @@ namespace SchoolManager
     {
         private const int DefaultIncome = 10000;
 
-        public event EventHandler<Complaint>? ComplaintRaised;
+        public event EventHandler<ComplaintEventArgs>? ComplaintRaised;
 
         public Receptionist(string name, Address address, string phoneNumber, int income = DefaultIncome)
             : base(name, address, phoneNumber)
@@ -20,13 +20,15 @@ namespace SchoolManager
 
         public void HandleComplaint(string complaintText)
         {
-            Complaint complaint = new Complaint();
-            complaint.ComplaintTime = DateTime.Now;
-            complaint.ComplaintRaised = complaintText;
+            if (string.IsNullOrWhiteSpace(complaintText))
+                throw new ArgumentException("Complaint cannot be empty.", nameof(complaintText));
 
+            ComplaintEventArgs complaint = new ComplaintEventArgs(complaintText);
             ComplaintRaised?.Invoke(this, complaint);
+
+            Console.WriteLine($"Complaint handled by {Name} at {complaint.ComplaintTime}: {complaint.ComplaintText}");  
         }
-        
+
         public int Income
         {
             get => _income;
--- SchoolManagerApp/Root/Complaint.cs
+++ SchoolManagerApp/Root/Complaint.cs
@@ -1,8 +1,19 @@
 namespace SchoolManager
 {
-       public class Complaint : EventArgs
+    public class ComplaintEventArgs : EventArgs
     {
         public DateTime ComplaintTime { get; set; }
         public string ComplaintRaised { get; set; } = string.Empty;
+
+        public ComplaintEventArgs(string complaintRaised)
+        {
+            ComplaintTime = DateTime.Now;
+            ComplaintRaised = complaintRaised;
+        }
+
+        public override string ToString()
+        {
+            return $"Complaint raised at {ComplaintTime}: {ComplaintRaised}";
+        }
     }
 }
